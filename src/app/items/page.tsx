abd9f8c2614f8d96e76ad603041ec3100b25f36c
rudwnok <rudwnok123@naver.com>
2026-08-12 18:44:31 +0900

    seo테스트

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,4 +1,15 @@
 import ItemList from './ItemList';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '올포유 - 아이템 ',
+  description: '포켓몬 아이템 목록, 올포유 포켓몬 아이템 목록, 포켓몬 아이템 종류',
+  openGraph: {
+    title: '올포유 - 아이템',
+    description: '포켓몬 아이템 목록, 올포유 포켓몬 아이템 목록, 포켓몬 아이템 종류',
+    url: 'https://allpoyou.vercel.app/items',
+  },
+};
 
 const ItemPage = () => {
   return <ItemList />;
--- src/app/make-team/layout.tsx
+++ src/app/make-team/layout.tsx
@@ -2,12 +2,12 @@ export const dynamic = 'force-dynamic';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "팀 만들기",
-  description: "포켓몬을 한개만 선택해도 타입을 보완하는 포켓몬을 추천해줍니다. ",
-  icons: {
-    icon: "/images/러브볼.ico",
-    shortcut: "/images/러브볼.ico",
-    apple: "/images/러브볼.ico",
+  title: "올포유 - 팀 만들기",
+  description: "포켓몬 팀 만들기, 포켓몬 추천, 포켓몬 팀 플랜, 포켓몬을 한개만 선택해도 타입을 보완하는 포켓몬을 추천해줍니다. ",
+  openGraph: {
+    title: "올포유 - 팀 만들기",
+    description: "포켓몬을 한개만 선택해도 타입을 보완하는 포켓몬을 추천해줍니다. ",
+    url: "https://allpoyou.vercel.app/make-team",
   },
 };
 
